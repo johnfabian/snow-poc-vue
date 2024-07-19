10c50b37c6914eb903d54672d745f437c2df94df
John Fabian <jfabian007@gmail.com>
2024-07-19 11:05:55 +0200

    sample demo app for snow completed

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -12,7 +12,7 @@
     </div>
   </div>
   <div class="grid">
-    <div class="col" >
+    <div class="col">
       <ChartWrapper title="Vertical Bar Chart">
         <VerticalBarChart />
       </ChartWrapper>
@@ -20,9 +20,8 @@
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import ChartWrapper from '@/components/charts/ChartWrapper.vue'
-import BasicChart from '@/components/charts/BasicChart.vue'
 import PieChart from '@/components/charts/PieChart.vue'
 import DoughnutChart from '@/components/charts/DoughnutChart.vue'
 import VerticalBarChart from '@/components/charts/VerticalBarChart.vue'
